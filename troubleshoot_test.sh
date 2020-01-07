8fe16d95f8f08d4a66aa86a98109a60f2224ed5e
louiscklaw <louiscklaw@gmail.com>
2020-01-07 23:40:16 +0800

    update travis and test,

--- troubleshoot_test.sh
+++ troubleshoot_test.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-pipenv sync
 # pipenv run python3 test.py
+
 cd parser/JLCPCB
+
+pipenv sync
 pipenv run python3 parse.py test/resistor_only.xls
+
+diff -r test/expected_result test/results
