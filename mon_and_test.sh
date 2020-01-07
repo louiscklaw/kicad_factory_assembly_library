9fb8fa859967f3b7e6d889adc0935b107e93cee6
louiscklaw <louiscklaw@gmail.com>
2020-01-07 23:05:26 +0800

    update travis and test,

--- mon_and_test.sh
+++ mon_and_test.sh
@@ -2,4 +2,4 @@
 
 
 echo 'running test.sh'
-nodemon -w '.' --ext '*.xls' --ext '*.py'   --delay 300ms --exec './test.py'
+nodemon -w '.' --ext '*.xls' --ext '*.py'   --delay 300ms --exec 'pipenv run python3 ./test.py'
