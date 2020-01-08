e93da9f6dfd6c98a0efc951a5a3e88f51983bb06
louiscklaw <lousicklaw@gmail.com>
2020-01-08 19:23:20 +0800

    update test,

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ cd parser/JLCPCB
 
 pipenv sync
 
-pipenv run python3 parse.py test/resistor_only.xls parser/JLCPCB/test/results
-pipenv run python3 parse.py test/capacitors_only.xls parser/JLCPCB/test/results
+pipenv run python3 parse.py test/resistor_only.xls test/results
+pipenv run python3 parse.py test/capacitors_only.xls test/results
 
 
 diff -r test/expected_result test/results
