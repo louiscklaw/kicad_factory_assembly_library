65b60a133da6e1618a7616032b174efa5793c0d7
louiscklaw <lousicklaw@gmail.com>
2020-01-09 13:19:02 +0800

    update test case,

--- test_all.py
+++ test_all.py
@@ -9,14 +9,38 @@ CURRENT_RESULT_DIR = 'test/results'
 file_list = [
   'jlcpcb_resistors.lib','jlcpcb_resistors.dcm',
   'jlcpcb_capacitors.lib','jlcpcb_capacitors.dcm',
-  'jlcpcb_inductors_chokes_transformers.lib','jlcpcb_inductors_chokes_transformers.dcm'
+  'jlcpcb_inductors_chokes_transformers.lib','jlcpcb_inductors_chokes_transformers.dcm',
+  'jlcpcb_diodes.lib','jlcpcb_diodes.dcm',
   ]
 
 def try_build():
   commands = [
-    'pipenv run python3 parse.py test/resistor_only.xls test/results',
+    # 'pipenv run python3 parse.py test/amplifiers_only.xls test/results',
+    # 'pipenv run python3 parse.py test/analog_ics_only.xls test/results',
+    # 'pipenv run python3 parse.py test/battery_products_only.xls test/results',
+    # 'pipenv run python3 parse.py test/capacitors_only1.xls test/results',
     'pipenv run python3 parse.py test/capacitors_only.xls test/results',
+    # 'pipenv run python3 parse.py test/crystals_only.xls test/results',
+    'pipenv run python3 parse.py test/diodes_only.xls test/results',
+    # 'pipenv run python3 parse.py test/driver_ics_only.xls test/results',
+    # 'pipenv run python3 parse.py test/embedded_peripheral_ics_only.xls test/results',
+    # 'pipenv run python3 parse.py test/embedded_processors_controllers_only.xls test/results',
+    # 'pipenv run python3 parse.py test/filters_only.xls test/results',
+    # 'pipenv run python3 parse.py test/first_category_only.xls test/results',
+    # 'pipenv run python3 parse.py test/fuses_only.xls test/results',
     'pipenv run python3 parse.py test/inductors_chokes_transformers_only.xls test/results',
+    # 'pipenv run python3 parse.py test/interface_ics_only.xls test/results',
+    # 'pipenv run python3 parse.py test/logic_ics_only.xls test/results',
+    # 'pipenv run python3 parse.py test/memory_only.xls test/results',
+    # 'pipenv run python3 parse.py test/optocouplers_leds_infrared_only.xls test/results',
+    # 'pipenv run python3 parse.py test/others_only.xls test/results',
+    # 'pipenv run python3 parse.py test/power_management_ics_only.xls test/results',
+    # 'pipenv run python3 parse.py test/pushbutton_switches_relays_only.xls test/results',
+    # 'pipenv run python3 parse.py test/resistor_only_small.xls test/results',
+    'pipenv run python3 parse.py test/resistor_only.xls test/results',
+    # 'pipenv run python3 parse.py test/rf_radio_only.xls test/results',
+    # 'pipenv run python3 parse.py test/sensors_only.xls test/results',
+    # 'pipenv run python3 parse.py test/transistors_only.xls test/results',
   ]
   try:
     for command in commands:
