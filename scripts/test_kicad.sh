afe0d6db707fb2ae3dc7a3bc74b6999397676689
louiscklaw <lousicklaw@gmail.com>
2020-01-10 20:37:45 +0800

    update and test,

--- scripts/test_kicad.sh
+++ scripts/test_kicad.sh
@@ -6,27 +6,9 @@ cd _util/kicad-library-utils/schlib
 
 rm -rf /home/logic/_workspace/kicad_factory_assembly_library/_util/kicad-library-utils/schlib/tmp/*
 
-./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_amplifiers.lib &
-./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_analog_ics.lib &
-./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_battery_products.lib &
-./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_capacitors.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_crystals.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_diodes.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_driver_ics.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_embedded_peripheral_ics.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_embedded_processors_controllers.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_filters.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_fuses.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_inductors_chokes_transformers.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_logic_ics.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_memory.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_optocouplers_leds_infrared.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_power_management_ics.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_pushbutton_switches_relays.lib &
-./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_resistors.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_rf_radio.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_sensors.lib &
-# ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_transistors.lib
+for file in $LIB_RESULT_DIR/*.lib; do
+  ./test_schlib.sh $LIB_RESULT_DIR/jlcpcb_amplifiers.lib &
+done
 
 wait
 
