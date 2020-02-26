8b46f30793421668bf4843858cf5013f10300855
louiscklaw <louiscklaw@gmail.com>
2020-02-26 19:49:55 +0800

    fix: fix for space in footprint field,

--- parser/SZ-JLC/src/config.py
+++ parser/SZ-JLC/src/config.py
@@ -4,7 +4,10 @@ import os,sys,re
 from pprint import pprint
 
 HOME_DIR = os.path.dirname(__file__)
+PROJ_HOME = os.path.join(HOME_DIR,'../../..')
 INPUT_DIR = os.path.join(HOME_DIR,'input')
 OUTPUT_DIR = os.path.join(HOME_DIR,'output')
+
+DCM_LIB_OUTPUT_DIR = os.path.join(PROJ_HOME, 'output')
 
 INPUT_XLS_FILEPATH = os.path.join(INPUT_DIR,'exportComponentList.xls')
